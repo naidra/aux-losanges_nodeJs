0f547be1ed69415b38aaacdbc775a8f32915db26
Ardian Sallauka <ardi_c_cc@hotmail.com>
2023-04-11 06:28:12 +0200

    Email config changed

--- controllers/eventReportsController.js
+++ controllers/eventReportsController.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const EventReport = mongoose.model('EventReport');
 const Mailjet = require('node-mailjet');
-
+const { promisify } = require('es6-promisify');
+const mailjetTransport = require('nodemailer-mailjet-transport');
 
 exports.eventReportsPage = async (req, res) => {
     const eventReports = await EventReport.find();
@@ -30,46 +31,32 @@ exports.eventReportEditPost = async (req, res) => {
         runValidators: true
     }).exec();
 
-    const mailjet = Mailjet.apiConnect(
-        process.env.MAIL_MAILJET_KEY,
-        process.env.MAIL_MAILJET_SECRET,
-    );
 
-    const request = mailjet
-        .post('send', { version: 'v3.1' })
-        .request({
-            Messages: [
-                {
-                    From: {
-                        Email: "ardiansallauka@gmail.com",
-                        Name: "Aux-losanges Contact"
-                    },
-                    To: [
-                        {
-                            Email: req.params.Email,
-                            Name: req.params.Name
-                        }
-                    ],
-                    Subject: "Data about your event has changed",
-                    TextPart: "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
-                    HTMLPart: `<div>
-                        <p>Data for the event has changed to:</p>
-                        <p>NAME: ${req.params.Name}</p>
-                        <p>NAME: ${req.params.Email}</p>
-                        <p>NAME: ${req.params.Seats}</p>
-                        <p>NAME: ${req.params.Status}</p>
-                        <p>NAME: ${req.params.EventDate}</p>
-                    </div>`
-                }
-            ]
-        });
+    const transport = nodemailer.createTransport(mailjetTransport({
+        auth: {
+            apiKey: process.env.MAIL_MAILJET_KEY,
+            apiSecret: process.env.MAIL_MAILJET_SECRET
+        }
+    }));
+    
+    const mailOptions = {
+        from: 'Ardian Sallauka <ardiansallauka@gmail.com>',
+        to: req.params.Email,
+        subject: "Data about your event has changed",
+        Null,
+        text: `<div>
+            <p>Data for the event has changed to:</p>
+            <br>
+            <p>NAME: ${req.params.Name}</p>
+            <p>Email: ${req.params.Email}</p>
+            <p>Seats: ${req.params.Seats}</p>
+            <p>Status: ${req.params.Status}</p>
+            <p>EventDate: ${req.params.EventDate}</p>
+        </div>`
+    };
 
-    request.then((result) => {
-            console.log('result.body: ', result.body)
-        })
-        .catch((err) => {
-            console.log('err.statusCode: ', err.statusCode)
-        });
+    const sendMail = promisify(transport.sendMail).bind(transport);
+    sendMail(mailOptions);
 
     req.flash('success', `Successfully changed EventReport.`);
     res.redirect('https://aux-losanges.ch/event-reports/');
